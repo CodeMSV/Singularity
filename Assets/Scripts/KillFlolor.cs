b959479c71623a5a65f7501f2baecc0d603cd761
savamidev <migue.626@gmail.com>
2025-11-13 00:48:15 +0100

    Refactor code

--- Assets/Scripts/KillFlolor.cs
+++ Assets/Scripts/KillFlolor.cs
@@ -4,7 +4,6 @@ public class KillFloor : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        // 1. Si el JUGADOR toca el suelo, activa el Game Over
         if (other.CompareTag("Player"))
         {
             if (GameManager.Instance != null)
@@ -14,8 +13,6 @@ public class KillFloor : MonoBehaviour
         }
         else
         {
-            // 2. Si es CUALQUIER OTRA COSA (enemigos, balas, escombros),
-            // simplemente destrúyelo para limpiar la escena.
             Destroy(other.gameObject);
         }
     }
