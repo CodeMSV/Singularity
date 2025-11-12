8028dfeede16fab8d248057e26354e59dc91953c
savamidev <migue.626@gmail.com>
2025-11-13 00:47:56 +0100

    Refactor code

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -1,24 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.Collections; // <-- ¡Importante! Necesario para las Corutinas
+using System.Collections;
 
-public class AutoSceneChanger : MonoBehaviour // Puedes cambiar el nombre de la clase
+public class AutoSceneChanger : MonoBehaviour
 {
-    // Esta función se llama automáticamente una vez 
-    // en el primer frame que este script está activo.
     void Start()
     {
-        // Inicia la corutina que esperará 1 segundo.
         StartCoroutine(LoadSceneAfterDelay(1.0f));
     }
 
-    // Esta es la Corutina
     private IEnumerator LoadSceneAfterDelay(float delay)
     {
-        // Espera por el número de segundos especificado
         yield return new WaitForSeconds(delay);
 
-        // Después de esperar, carga la escena
         SceneManager.LoadScene("InfoScene");
     }
 }
