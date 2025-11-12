74fd82e283accaf68b62997a467736a70a866e41
savamidev <migue.626@gmail.com>
2025-11-13 00:47:20 +0100

    Refactor code

--- Assets/Scripts/InfoGameController.cs
+++ Assets/Scripts/InfoGameController.cs
@@ -3,10 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class InfoGameController : MonoBehaviour
 {
-    // Esta función la llama el botón "Jugar"
     public void LoadGameScene()
     {
-        // Carga la Escena 4: "Juego"
         SceneManager.LoadScene("Juego");
     }
 }
